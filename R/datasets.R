402d05cfb8cf704769cf391fb0cdfdb5037565c5
Jorge Marmanillo <jlmarmanillo@gmail.com>
2026-08-16 15:04:02 -0500

    Initial commit

---
+++ R/datasets.R
@@ -0,0 +1,74 @@
+#' @title STARplus Dataset
+#' @description STAR participants plus nonexperimental controls.
+#' @format Data frame: 13,381 rows, 56 variables.
+"STARplus"
+
+#' @title NELS Dataset
+#' @description National Education Longitudinal Study (NELS) of 1988.
+#' @format Data frame: 500 rows, 48 variables.
+"NELS"
+
+#' @title MathAchieve Dataset
+#' @description Mathematics achievement scores.
+#' @format Data frame: 7,185 rows, 6 variables.
+"MathAchieve"
+
+#' @title classroom Dataset
+#' @description Classroom data in Chapter 4 of the Study of Instructional Improvement.
+#' @format Data frame: 1,190 rows, 12 variables.
+"classroom"
+
+#' @title schools Dataset
+#' @description Schools data set (NELS-88) for multilevel modeling.
+#' @format Data frame: 260 rows, 19 variables.
+"schools"
+
+#' @title apipop Dataset
+#' @description Student performance in California schools.
+#' @format Data frame: 6,194 rows, 37 variables.
+"apipop"
+
+#' @title CASchools Dataset
+#' @description California Test Score Data on school performance and demographics.
+#' @format Data frame: 420 rows, 14 variables.
+"CASchools"
+
+#' @title schoolProgram Dataset
+#' @description High School and Beyond Survey data on student programs.
+#' @format Data frame: 200 rows, 11 variables.
+"schoolProgram"
+
+#' @title CollegeDistance Dataset
+#' @description College distance data from the High School and Beyond survey.
+#' @format Data frame: 4,739 rows, 14 variables.
+"CollegeDistance"
+
+#' @title school Dataset (CollegeScorecard)
+#' @description Information about institutions in the U.S. College Scorecard.
+#' @format Data frame: 11,300 rows, 25 variables.
+"school"
+
+#' @title STUDENT Dataset
+#' @description College GPAs of students in an introductory statistics class.
+#' @format Data frame: 607 rows, 19 variables.
+"STUDENT"
+
+#' @title FirstYearGPA Dataset
+#' @description Predicting first-year college GPA for students.
+#' @format Data frame: 219 rows, 10 variables.
+"FirstYearGPA"
+
+#' @title ExamScores Dataset
+#' @description Exam results for six inner London Education Authorities.
+#' @format Data frame: 4,059 rows, 10 variables.
+"ExamScores"
+
+#' @title pisausa Dataset
+#' @description 2009 PISA results for students in the United States.
+#' @format Data frame: 5,233 rows, 65 variables.
+"pisausa"
+
+#' @title EducationLiteracy Dataset
+#' @description Education spending and literacy rates for countries.
+#' @format Data frame: 193 rows, 4 variables.
+"EducationLiteracy"
